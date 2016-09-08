3ff8138a14ea083849b87b8c70a6ce33b1639e04
carrollroberta <carroll.roberta@uwlax.edu>
2016-09-07 21:18:21 -0500

    Part 1:  Changes to R  (see notes below.....)
    lastsave.txt shows the R console results.
    assignment1_r_CarrollR.docx contains the R assignments with answers.
    assignment1-CarrollR.R is the r-script for running assignments 1.1 -
    1.9.
    R Standard Deviation is the R Help Documentation for Standard Deviation.

---
+++ assignment1-CarrollR.R
@@ -0,0 +1,35 @@
+#1.1
+2015^(1/3)
+
+#1.2
+abs(5.7-6.8)/.58
+
+#1.3
+a = 1:12
+a   #(this will print a, so you can paste it into your homework; do this each time)
+
+#1.4
+b = c(1, 3, 5, 7, 9, 11)
+b
+
+#1.5
+c = seq(1,11, 2)
+c
+
+#1.6
+ln.a = log(a)
+ln.a
+
+#1.7
+oddNumbers = c(1, 3, 5, 7, 9, 11)
+oddNumbers
+squaresOddNumbers = oddNumbers ^ 2
+squaresOddNumbers
+
+#1.8
+?sd 
+
+#1.9
+Name = "Roberta"
+paste("My name is", Name)
+
